5cfbff17f530e96fbf41c82e831e7828d6ab4096
Dakkahi <yuyasik418@gmail.com>
2023-08-30 10:58:50 +0900

    ビット全探索まで追加

---
+++ chapter2/practice/id008.cpp
@@ -0,0 +1,18 @@
+//全探索
+#include<iostream>
+using namespace std;
+
+int N, S;
+long long ans = 0;
+
+int main(){
+    cin >> N >> S;
+    for(int i=1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            if(i+j <= S) ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+
